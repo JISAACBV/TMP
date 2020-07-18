1318ad46cb6eaf5463e5da43ebd9088939587f54
JISAACBV <jisaacbv@gmail.com>
2020-07-17 21:38:48 -0400

    Jose

---
+++ assets/mail/enviar.php
@@ -0,0 +1,13 @@
+<?php
+    $destino= "jisaacbv@gmail.com";
+    $nombre= $_POST["name"];
+    $email= $_POST["email"];
+    $phone= $_POST["phone"];
+    $mensaje= $_POST["menssage"];
+    /* $contenido =  " Nombre: " . $nombre . "\nCorreo: " . $email . "\nTeléfono: " . $phone . "\nMensaje: " . $mensaje; */
+    $contenido = "Recibiste este mensaje de tu Formulario TMP.\n\n"."Acá los detalles:\n\nNombre: $nombre\n\nCorreo: $email\n\nTeléfono: $phone\n\nMensaje:\n$mensaje";
+
+    mail($destino,"Contacto",$contenido);
+
+
+?>  
